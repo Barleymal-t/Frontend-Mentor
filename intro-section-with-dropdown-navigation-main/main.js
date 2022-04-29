08a4b225fef200da83574d96f573b7e4d96dd5db
Nii <niiodartey10@gmail.com>
2022-04-29 22:21:11 +0100

    started with mobile

--- intro-section-with-dropdown-navigation-main/main.js
+++ intro-section-with-dropdown-navigation-main/main.js
@@ -1,14 +1,15 @@
-document.addEventListener('click',(e)=> {
-    const isDropDown = e.target.matches('data-dropdown-button')
-    if(!isDropDown && e.target.closest("[data-dropdown]") != null) return
+// document.addEventListener('click', (e) => {
+//     const isDropDown = e.target.matches('[data-dropdown-button]')
+//     // if (!isDropDown && e.target.closest("[data-dropdown]") != null) return
 
-    let currentDropDown
-    if(isDropDown){
-        currentDropDown = e.target.closest("[data-dropdown]")
-        currentDropDown.classList.toggle('active')
-    }
-    document.querySelectorAll("[data-dropdown].active").forEach(dropDown=>{
-    if(dropDown === currentDropDown) return
-    dropDown.classList.remove("active")
-})
-})
+//     let currentDropDown
+//     if (isDropDown) {
+//         currentDropDown = e.target.closest("[data-dropdown]")
+//         currentDropDown.classList.toggle('active')
+//     }
+//     document.querySelectorAll("[data-dropdown].active").forEach(dropDown => {
+//         if (dropDown === currentDropDown) {
+//             dropDown.classList.remove("active")
+//         }
+//     })
+// })
